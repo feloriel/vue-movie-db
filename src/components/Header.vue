5fed9160a7911c3a988cefcd6c73e55c77facde0
Irina Lepekhina <irina.lepekhina95@gmail.com>
2019-07-10 17:37:52 +0300

    computed properties in vuejs

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header>
-    <h1>{{ title }}</h1>
+    <h1>{{ newTitle }}</h1>
   </header>
 </template>
 
@@ -9,6 +9,19 @@
     name: 'Header',
     props: {
       title: String
+    },
+    data() {
+      return {
+        name: 'Scott'
+      }
+    },
+    computed: {
+      newTitle: function () {
+        if (this.title === 'hello') {
+          return this.title;
+        }
+        return this.name;
+      }
     }
   }
 </script>
