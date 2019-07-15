010c4e28003feb2fd1f4afaf3dbdfe92a11a56e3
Irina Lepekhina <irina.lepekhina95@gmail.com>
2019-07-15 14:55:51 +0300

    hitting the moviedb api with methods

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <template>
   <div id="app" v-if="status === 'Ready'">
     <Header :title="title" />
-    <img alt="Vue logo" src="./assets/logo.png">
-    <h1>{{ hello }}</h1>
-    <HelloWorld 
-      msg="Welcome to Your Vue.js App" 
-    />
+    <MoviesList />
   </div>
   <div v-else-if="status === 'Loading'">
     Loading
@@ -16,8 +12,8 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
 import Header from './components/Header.vue';
+import MoviesList from './components/MoviesList.vue';
 
 export default {
   name: 'app',
@@ -30,8 +26,8 @@ export default {
     };
   },
   components: {
-    HelloWorld,
-    Header
+    Header,
+    MoviesList
   }
 }
 </script>
