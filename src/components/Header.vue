4742ffd7bcc81f9a152ee6de9ef16cb15c51ddbf
Irina Lepekhina <irina.lepekhina95@gmail.com>
2019-07-18 14:32:28 +0300

    page transitions

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,6 @@
 <template>
   <header>
     <h1><router-link to="/">{{ title }}</router-link></h1>
-    <transition name="fade">
-      <h1 v-if="show">Animated</h1>
-    </transition>
-    <button @click="show = !show">Show/Hide</button>
   </header>
 </template>
 
@@ -28,25 +24,7 @@ header {
   padding: 20px;
   color: white;
 }
-
 h1 {
   margin: 0;
 }
-
-.fade-enter-acive,
-.fade-leave-active {
-  transition: all 0.3s ease;
-}
-
-.fade-enter,
-.fade-leave-to {
-  opacity: 0;
-  transform: scale(0);
-}
-
-/* name-enter -> name-enter-to */
-/* name-enter-active */
-
-/* name-leave -> name-leave-to */
-/* name-leave-active */
 </style>
